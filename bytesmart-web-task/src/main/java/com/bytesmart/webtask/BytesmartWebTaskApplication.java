51c5830cc46f3e9b072b7a3e1cb77fd4f5be44c7
kotann <13594321+kotann@user.noreply.gitee.com>
2024-03-16 11:19:22 +0800

    bytesmart web task修改正常调用查看数据

--- bytesmart-web-task/src/main/java/com/bytesmart/webtask/BytesmartWebTaskApplication.java
+++ bytesmart-web-task/src/main/java/com/bytesmart/webtask/BytesmartWebTaskApplication.java
@@ -1,20 +1,18 @@
 package com.bytesmart.webtask;
 
-//import com.bytesmart.springsecurity.annotation.EnableRyFeignClients;
 import com.bytesmart.common.swagger.annotation.EnableCustomSwagger2;
 import com.bytesmart.springsecurity.annotation.EnableCustomConfig;
+import com.bytesmart.springsecurity.annotation.EnableRyFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableCustomConfig
 @EnableCustomSwagger2
-//@EnableRyFeignClients
-@SpringBootApplication(scanBasePackages = {"com.bytesmart.web*"})
+@EnableRyFeignClients
+@SpringBootApplication(scanBasePackages = {"com.bytesmart.*"})
 public class BytesmartWebTaskApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BytesmartWebTaskApplication.class, args);
         System.out.println("byteSmart 任务认证模块启动成功");
     }
-
 }
--- bytesmart-web-task/src/main/java/com/bytesmart/webtask/controller/BytesmartTasksController.java
+++ bytesmart-web-task/src/main/java/com/bytesmart/webtask/controller/BytesmartTasksController.java
@@ -6,6 +6,7 @@ import com.bytesmart.common.core.web.page.TableDataInfo;
 import com.bytesmart.webtask.domain.BytesmartTasks;
 import com.bytesmart.webtask.service.IBytesmartTasksService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,19 @@ public class BytesmartTasksController extends BaseController {
     public AjaxResult getInfo(@PathVariable Long taskId)
     {
         return success(bytesmartTasksService.selectTaskBytaskId(taskId));
+    }
+
+
+    // 测试接口：
+    // 1、用postman直接调用，不传token看下是否提示没权限，如果不可以是正常的。
+    // 2、然后用token请求看看
+    @GetMapping("test")
+    public AjaxResult test(){
+        AjaxResult ajax = AjaxResult.success();
+        ajax.put("user", "A");
+        ajax.put("roles", "B");
+        ajax.put("permissions", "C");
+        return ajax;
     }
 
 }
